5692db95978fc9eddaee93672cdb907991469810
ctnkaan <kaan.taskingenc@gmail.com>
2022-12-24 16:54:28 +0300

    🔨 update & add: date, reactions, comment count

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import { buildSchema } from 'graphql'
+import moment from 'moment'
 import { Iinput, Icomment } from './interfaces'
 
 const app = express()
 
-const sampleData = {
+const db = {
     posts: [
         {
             id: 1,
             content: 'Welcome to learning GraphQL!',
-            thumbsUp: 0,
+            reactions: {
+                thumbsUp: 0,
+                thumbsDown: 0,
+                rocket: 0,
+                heart: 0,
+            },
             comments: [
                 {
                     id: 1,
                     content: 'This is a comment',
-                    thumbsUp: 0,
+                    reactions: {
+                        thumbsUp: 0,
+                        thumbsDown: 0,
+                        rocket: 0,
+                        heart: 0,
+                    },
                     comments: [],
                 },
             ],
-        },
-        {
-            id: 2,
-            content: 'This is a second post',
-            thumbsUp: 0,
-            comments: [
-                {
-                    id: 1,
-                    content: 'This is a comment',
-                    thumbsUp: 0,
-                    comments: [],
-                },
-            ],
-        },
+            commentCount: 1,
+            createdAt: moment().fromNow()
+        }
     ],
 }
 
@@ -62,38 +62,54 @@ const Schema = buildSchema(`#graphql
     type Post {
         id: Int!
         content: String!
-        thumbsUp: Int
+        reactions: Reactions
         comments: [Comment]
+        commentCount: Int
+        createdAt: String
     }
 
     type Comment {
         id: Int
         content: String
-        thumbsUp: Int
+        reactions: Reactions
         comments: [Comment]
+    }
+
+    type Reactions {
+        thumbsUp: Int
+        thumbsDown: Int
+        rocket: Int
+        heart: Int
     }
 `)
 
 const Resolvers = {
-    getAllPosts: () => sampleData.posts,
+    getAllPosts: () => db.posts,
 
     getOnePost: ({ id }: { id: number }) =>
-        sampleData.posts.find((post: any) => post.id === id),
+        db.posts.find((post: any) => post.id === id),
 
     createPost: ({ input }: { input: Iinput }) => {
         const newPost = {
-            id: sampleData.posts.length + 1,
+            id: db.posts.length + 1,
             content: input.content,
-            thumbsUp: 0,
+            reactions: {
+                thumbsUp: 0,
+                thumbsDown: 0,
+                rocket: 0,
+                heart: 0,
+            },
+            commentCount: 0,
             comments: [],
+            createdAt: moment().fromNow()
         }
-        sampleData.posts.push(newPost)
+        db.posts.push(newPost)
         return newPost
     },
 
     createCommentForPost: ({ comment }: { comment: Icomment }) => {
         //find the post with the comment id
-        const post = sampleData.posts.find(
+        const post = db.posts.find(
             (post: any) => post.id === comment.postId
         )
 
@@ -104,25 +120,50 @@ const Resolvers = {
         const newComment = {
             id: post.comments.length + 1,
             content: comment.content,
-            thumbsUp: 0,
+            reactions: {
+                thumbsUp: 0,
+                thumbsDown: 0,
+                rocket: 0,
+                heart: 0,
+            },
             comments: [],
         }
 
         //add the comment to the post
         post.comments.push(newComment);
+
+        //update the comment count
+        post.commentCount = post.comments.length;
+
         return newComment;
     },
 
     changeVotePost: ({ id, vote }: { id: number, vote: string }) => {
-        const post = sampleData.posts.find((post: any) => post.id === id)
+        const post = db.posts.find((post: any) => post.id === id)
+        
         if (post === undefined) throw new Error('Post not found with id: ' + id)
 
-        if (vote === 'up') post.thumbsUp++
-        else if (vote === 'down') post.thumbsUp--
-        else throw new Error('Vote must be up or down')
+        switch (vote) {
+            case 'up':
+                post.reactions.thumbsUp++
+                break
+            case 'down':
+                post.reactions.thumbsUp--
+                break
+            case "rocket":
+                post.reactions.rocket++
+                break
+            case "heart":
+                post.reactions.heart++
+                break
+            default:
+                throw new Error('Vote must be up, down, rocket or heart')
+        }
 
         return post
-    }
+    },
+
+    
 }
 
 app.use(
